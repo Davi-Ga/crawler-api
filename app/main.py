d66a3f7371fb70a3bd6585fb119945367ac8afa2
Davi-Ga <davigaldinoky@gmail.com>
2023-08-01 22:51:44 -0300

    feat: handler exception adicionado

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from config import config
 from router.endpoints import api_router
-
+from crawler.exceptions import JurisprudenceNotFoundException,InternalServerException
+from fastapi.responses import JSONResponse
+from fastapi import Request
+from crawler.handlers import internal_server_error_handler,jurisprudence_not_found_handler
 
 app = FastAPI(
     title=config.PROJECT_NAME, 
@@ -12,10 +15,12 @@ app = FastAPI(
 
 app.include_router(api_router,prefix=config.API_PREFIX)
 
+
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
-
+app.add_exception_handler(InternalServerException, internal_server_error_handler)
+app.add_exception_handler(JurisprudenceNotFoundException, jurisprudence_not_found_handler)
 
