60e3fd81d2187393520b1189b7e0df63b774675f
Davi-Ga <davigaldinoky@gmail.com>
2023-08-01 22:52:13 -0300

    feat: exception adicionada

--- app/crawler/api.py
+++ app/crawler/api.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
-
 from crawler.core import get_jurisprudences
+from .exceptions import JurisprudenceNotFoundException,InternalServerException
+from fastapi.responses import JSONResponse
+from fastapi import Request
 
 router=APIRouter()
 
 @router.get('/search/{name}')
 async def jurisprudences(name:str):
-    data = await get_jurisprudences(name)
-    return data
+    
+    if data := await get_jurisprudences(name):
+        return JSONResponse(data)
+    else:
+        raise JurisprudenceNotFoundException(name=name)
+
+
