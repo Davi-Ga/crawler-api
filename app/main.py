2e62cbb4e3c54f264f7df513412af6f1e92d7e14
Davi-Ga <davigaldinoky@gmail.com>
2023-08-14 18:13:42 -0300

    fix: handler removida

--- app/main.py
+++ app/main.py
@@ -1,26 +1,17 @@
 from fastapi import FastAPI
-from config import config
+from config import config_api
 from router.endpoints import api_router
-from crawler.exceptions import JurisprudenceNotFoundException,InternalServerException
-from fastapi.responses import JSONResponse
-from fastapi import Request
-from crawler.handlers import internal_server_error_handler,jurisprudence_not_found_handler
 
 app = FastAPI(
-    title=config.PROJECT_NAME, 
-    openapi_url=f"{config.API_PREFIX}/openapi.json",
-    version=config.VERSION,
-    debug=config.DEBUG
+    title=config_api.PROJECT_NAME, 
+    openapi_url=f"{config_api.API_PREFIX}/openapi.json",
+    version=config_api.VERSION,
+    debug=config_api.DEBUG
     )
 
-app.include_router(api_router,prefix=config.API_PREFIX)
-
-
+app.include_router(api_router,prefix=config_api.API_PREFIX)
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
-app.add_exception_handler(InternalServerException, internal_server_error_handler)
-app.add_exception_handler(JurisprudenceNotFoundException, jurisprudence_not_found_handler)
 
