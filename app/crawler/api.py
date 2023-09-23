f93b32227d5054e02a88df43fcddf1cf14f714db
Davi-Ga <davigaldinoky@gmail.com>
2023-09-23 12:35:20 -0300

    feat: salvando a requisição em csv

--- app/crawler/api.py
+++ app/crawler/api.py
@@ -3,6 +3,8 @@ from crawler.core import get_jurisprudences
 from .exceptions import JurisprudenceNotFoundException,InternalServerException
 from fastapi.responses import JSONResponse
 from fastapi import Request
+from .writer import csv_writer
+import json
 
 router=APIRouter()
 
@@ -10,6 +12,8 @@ router=APIRouter()
 async def jurisprudences(name:str):
     
     if data := await get_jurisprudences(name):
+        data_json=json.dumps(data)
+        csv_writer(json_string=data_json,name_file=name)
         return JSONResponse(data)
     else:
         raise JurisprudenceNotFoundException(name=name)
