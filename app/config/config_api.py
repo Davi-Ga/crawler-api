e67658f6a22439f7506480d16f0f22c6db91d569
Davi-Ga <davigaldinoky@gmail.com>
2023-09-23 12:34:31 -0300

    feat: email e password incluida

--- app/config/config_api.py
+++ app/config/config_api.py
@@ -9,3 +9,6 @@ DEBUG = config("DEBUG", cast=bool,default=True)
 PROJECT_NAME = config("PROJECT_NAME", cast=str,default="Jurisprudence Crawler")
 VERSION = config("VERSION", cast=str,default="1.0.0")
 
+EMAIL = config("EMAIL", cast=str,default="email")
+PASSWORD = config("PASSWORD", cast=str,default="password")
+
