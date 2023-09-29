d87387f04fe35259e196f107ff6ab146e4a22532
Davi-Ga <davigaldinoky@gmail.com>
2023-09-28 23:20:27 -0300

    feat: json saver incluido

--- app/crawler/api.py
+++ app/crawler/api.py
@@ -2,9 +2,7 @@ from fastapi import APIRouter
 from crawler.core import get_jurisprudences
 from .exceptions import JurisprudenceNotFoundException,InternalServerException
 from fastapi.responses import JSONResponse
-from fastapi import Request
-from .writer import csv_writer
-import json
+from .writer import json_saver
 
 router=APIRouter()
 
@@ -12,8 +10,8 @@ router=APIRouter()
 async def jurisprudences(name:str):
     
     if data := await get_jurisprudences(name):
-        data_json=json.dumps(data)
-        csv_writer(json_string=data_json,name_file=name)
+        json_saver(data_response=data,name_file=name)
+        
         return JSONResponse(data)
     else:
         raise JurisprudenceNotFoundException(name=name)
