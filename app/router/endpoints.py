202357b9a8347c086a2dcb21d464cd7390755375
Davi-Ga <davigaldinoky@gmail.com>
2023-09-30 22:20:36 -0300

    feat: router incluído para o matcher

--- app/router/endpoints.py
+++ app/router/endpoints.py
@@ -1,7 +1,9 @@
-from crawler import api
+from crawler import api as crawler_api
+from matcher import api as matcher_api
 
 from fastapi import APIRouter
 
 api_router=APIRouter()
 
-api_router.include_router(api.router,tags=['jurisprudences'])
+api_router.include_router(crawler_api.router, prefix="/crawler", tags=['crawler'])
+api_router.include_router(matcher_api.router, prefix="/matcher", tags=['matcher'])
