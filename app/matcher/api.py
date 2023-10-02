814bbe9496a887a41a7e1dbcf52394eb45c6b11d
zander404 <alexandrearthurpg@gmail.com>
2023-10-01 21:41:59 -0300

    init match case for JSON Files

---
+++ app/matcher/api.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, File, UploadFile, HTTPException
+import json
+from typing import Annotated
+from .core import readJSON, readPDF, readTXT
+
+router=APIRouter()
+
+@router.post('/search_file/')
+async def reader(file: UploadFile = File(..., description="Enviar Json do tipo \n{\n['title': 'etc...',\n 'body': '...']}",), words_bag: UploadFile = File(..., description="Enviar um arquivo TXT com as palavras a serem analisadas Ex: provimento\\n negar provimento (uma palavra em cada linha)")):
+    json_data = json.load(file.file)
+    file_content_type = file.content_type
+    file.file.seek(0,2)
+    file_size  = file.file.tell()
+    file.file.seek(0)
+ 
+
+    words_bag_type = words_bag.content_type
+    words_bag_file_size  = words_bag.file.tell()
+    words_bag.file.seek(0)
+    
+    
+    
+    if file_content_type  not in ["application/json"] and words_bag_type not in ["text/plain"]:
+        return HTTPException(status_code=400, detail="Invalid Type!!!")
+    
+    if file_size and words_bag_file_size <= 0:
+        return HTTPException(status_code=400, detail="File is Empty!!!")     
+    
+    return await readJSON(json_data, words_bag)
+
---
+++ app/matcher/core.py
@@ -0,0 +1,83 @@
+from PyPDF2 import PdfReader
+import re, json, unicodedata
+from typing import Annotated
+from fastapi import File, UploadFile
+import io
+
+def readPDF(file) -> [str, int]:
+    """
+    READ PDF: função para ler arquivos pdf e converter para texto(string)
+
+    PARAMS: 
+        file: PDF FILE = Arquivo PDF a ser lido.
+
+    RETURN 
+        clean_text: string = texto limpo limpo (sem escape keys)
+        page_num: int = numero de páginas no arquivo
+    """
+
+    clean_text = []
+    pdf_reader  = PdfReader(open(file, "rb"))
+    for page_num in range(len(pdf_reader.pages)):
+        text = pdf_reader.pages[page_num].extract_text()
+        clean_text.append(text.strip().replace("\n", '').lower())
+      
+
+    return clean_text, len(pdf_reader.pages)
+
+
+def readTXT(file, pattern):
+    """
+    READ TXT: função que vai ler o arquivo TXT e procurar se a palavra está presente no arquivo desejado
+
+    PARAMS:
+        file: TXT FILE = Arquivo Txt a ser analisado
+        pattern: string = Palavra a ser verificada se existe dentro do arquivo
+
+    RETURN:
+        NULL
+        Printar no console a palavra presente no documento
+        
+    """
+
+    with open(file, "r", encoding="utf-8") as f:
+        for _, line in enumerate(f, start=1):
+            line = line.lower()
+            if pattern in line:
+                print(f"A palavra {pattern} esta no Documento")
+                break
+
+        f.close()
+
+
+async def readJSON(file: dict, words_bag: UploadFile = File(...)) -> object:
+    """
+    DESCRIPTION:
+        READ JSON: função que vai ler o arquivo JSON e procurar se a palavra está presente no arquivo desejado,
+          retornando uma mensagem no Console, caso a palavra esteja presente.
+
+    PARAMS:
+        file: JSON FILE = Arquivo JSON a ser analisado
+        pattern: string = Palavra a ser verificada se existe dentro do arquivo
+
+    RETURN:
+        NULL
+        Printar no console a palavra presente no documento
+    """
+    
+    word_bag = words_bag.file.readlines()
+    file = file[0]['body']
+    result = []
+  
+    for word in word_bag:
+        word = io.StringIO(word.decode())
+        for line in word:
+            line = line.split()
+            pattern = " ".join(line)
+            pattern = pattern.strip()
+            
+            if re.search(pattern, file):
+                result.append(pattern)
+
+    
+    return {"data": result}
