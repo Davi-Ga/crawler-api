c823bf0b3a48084295821f41b32eab9a90f36afe
Davi-Ga <davigaldinoky@gmail.com>
2023-10-02 12:52:38 -0300

    feat: nova rota para a lista de nomes

--- app/crawler/api.py
+++ app/crawler/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from crawler.core import get_jurisprudences
 from .exceptions import JurisprudenceNotFoundException,InternalServerException
 from fastapi.responses import JSONResponse
-from .writer import json_saver
+from .utils import json_saver
 
 router=APIRouter()
 
@@ -15,5 +15,16 @@ async def jurisprudences(name:str):
         return JSONResponse(data)
     else:
         raise JurisprudenceNotFoundException(name=name)
+    
+@router.get('/list-search/{name}')
+async def jurisprudences(names:list):
+    
+    if data := await get_jurisprudences(name):
+        json_saver(data_response=data,name_file=name)
+        
+        return JSONResponse(data)
+    else:
+        raise JurisprudenceNotFoundException(name=name)
+
 
 
