d123efaec6822ee9c37337bb1a8bf0bfa1a25e9b
Davi-Ga <davigaldinoky@gmail.com>
2023-10-11 15:23:10 -0300

    feat: paginacao

--- app/crawler/utils.py
+++ app/crawler/utils.py
@@ -4,14 +4,14 @@ import csv
 import pandas as pd
 
 def json_saver(data_response:str,name_file:str) -> None:
-    open(f'app/data/negras{format_name_file(name_file)}.json', 'w').write(json.dumps(data_response))
+    open(f'app/data/negras/{format_name_file(name_file)}.json', 'w').write(json.dumps(data_response))
     
 def format_name_file(name_file:str) -> str:
     return name_file.replace(' ', '_').lower()
 
-def generate_url(name:str)->str:
+def generate_url(name:str,page:int=1)->str:
     processed_name=name.replace(" ","+")
-    return f"https://www.jusbrasil.com.br/jurisprudencia/busca?q={processed_name}"
+    return f"https://www.jusbrasil.com.br/jurisprudencia/busca?q=\"{processed_name}\"&p={page}"
 
 def get_names(person_color:str,analised_row:str,wanted_row:str,delimiter:str) -> List[str]:
     
