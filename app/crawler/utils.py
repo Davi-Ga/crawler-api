54b729cc98324aa705b510baf8551750f9aa5088
Davi-Ga <davigaldinoky@gmail.com>
2023-11-04 20:44:07 -0300

    feat: geração de url para peças processuais

--- app/crawler/utils.py
+++ app/crawler/utils.py
@@ -4,14 +4,18 @@ import csv
 import pandas as pd
 
 def json_saver(data_response:str,name_file:str) -> None:
-    open(f'app/data/negras/{format_name_file(name_file)}.json', 'w').write(json.dumps(data_response))
+    open(f'app/data/pardas/{format_name_file(name_file)}.json', 'w').write(json.dumps(data_response))
     
 def format_name_file(name_file:str) -> str:
     return name_file.replace(' ', '_').lower()
 
-def generate_url(name:str,page:int=1)->str:
+def generate_url_jurisprudence(name:str,page:int=1)->str:
     processed_name=name.replace(" ","+")
     return f"https://www.jusbrasil.com.br/jurisprudencia/busca?q=\"{processed_name}\"&p={page}"
+
+def generate_url_procedural_parts(name:str,page:int=1)->str:
+    processed_name=name.replace(" ","+")
+    return f"https://www.jusbrasil.com.br/pecas/busca?q={processed_name}&p={page}"
 
 def get_names(person_color:str,analised_row:str,wanted_row:str,delimiter:str) -> List[str]:
     
@@ -23,4 +27,4 @@ def get_names(person_color:str,analised_row:str,wanted_row:str,delimiter:str) ->
     print(f'Listando nomes de pessoas que se identificam com a cor {person_color}')
     return nomes
 
-print(get_names(person_color='negra',analised_row='raca',wanted_row='Nome_do_Servidor',delimiter=';'))
+print(get_names(person_color='parda',analised_row='raca',wanted_row='Nome_do_Servidor',delimiter=';'))
