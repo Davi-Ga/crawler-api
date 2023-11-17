ebc9a0ff9e8d99e2cc8cf2196e55f19b0f7d1d23
Davi-Ga <davigaldinoky@gmail.com>
2023-11-17 10:40:16 -0300

    feat: api ajustada para diferentes funções

--- app/crawler/api.py
+++ app/crawler/api.py
@@ -1,15 +1,25 @@
 from fastapi import APIRouter
-from ..crawler.core import get_jurisprudences
+from ..crawler.core import access_page_jurisprudences,access_page_procedural_parts
 from .exceptions import JurisprudenceNotFoundException,InternalServerException
 from fastapi.responses import JSONResponse
 from ..crawler.utils import json_saver
 
 router=APIRouter()
 
-@router.get('/search/{name}')
+@router.get('/search-jurisprudence/{name}')
 async def jurisprudences(name:str):
     
-    if data := await get_jurisprudences(name):
+    if data := await access_page_jurisprudences(name):
+        json_saver(data_response=data,name_file=name)
+        
+        return JSONResponse(data)
+    else:
+        raise JurisprudenceNotFoundException(name=name)
+    
+@router.get('/search-parts/{name}')
+async def procedural_parts(name:str):
+    
+    if data := await access_page_procedural_parts(name):
         json_saver(data_response=data,name_file=name)
         
         return JSONResponse(data)
