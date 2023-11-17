0338ed9b49ab19543297b8cca103ccbc211c6de5
Davi-Ga <davigaldinoky@gmail.com>
2023-11-16 21:05:47 -0300

    feat: funções reformuladas

--- app/crawler/utils.py
+++ app/crawler/utils.py
@@ -4,29 +4,29 @@ import csv
 import pandas as pd
 
 def json_saver(data_response:str,name_file:str) -> None:
-    open(f'app/data/pardas/{format_name_file(name_file)}.json', 'w').write(json.dumps(data_response))
+    formated_name=name_file.replace(' ', '_').lower()
+
+    open(f'app/data/negras/{formated_name}.json', 'w').write(json.dumps(data_response))
     
-def format_name_file(name_file:str) -> str:
-    return name_file.replace(' ', '_').lower()
-
-def generate_url_jurisprudence(name:str,page:int=1)->str:
+def generate_url(name:str,type_search:int,page:int=1)->str:
     processed_name=name.replace(" ","+")
-    return f"https://www.jusbrasil.com.br/jurisprudencia/busca?q=\"{processed_name}\"&p={page}"
-
-def generate_url_procedural_parts(name:str,page:int=1)->str:
-    processed_name=name.replace(" ","+")
-    return f"https://www.jusbrasil.com.br/pecas/busca?q={processed_name}&p={page}"
-
+    
+    if type_search == 1:
+        return f"https://www.jusbrasil.com.br/pecas/busca?q=\"{processed_name}\"&p={page}"
+    
+    elif type_search == 2:
+        return f"https://www.jusbrasil.com.br/jurisprudencia/busca?q=\"{processed_name}\"&p={page}"
+    
 def get_names(person_color:str,analised_row:str,wanted_row:str,delimiter:str) -> List[str]:
     
-    input_file=f'app/data/input/professores_ufop.csv'
+    input_file=f'app/data/input/docentes.csv'
     df = pd.read_csv(input_file,delimiter=delimiter)
      
-    nomes = df.loc[df[analised_row]==person_color.upper(),wanted_row].tolist()
+    nomes = df.loc[df[analised_row]==person_color,wanted_row].tolist()
         
     print(f'Listando nomes de pessoas que se identificam com a cor {person_color}')
     return nomes
 
 
-# print(get_names(person_color='negra',analised_row='raca',wanted_row='Nome_do_Servidor',delimiter=';'))
+print(get_names(person_color='Negra',analised_row='COR',wanted_row='NOME',delimiter='|'))
 
