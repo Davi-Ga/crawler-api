6b591c6aac7aca32266a2dd096ac8bf750bcfee1
Davi-Ga <contato@daviga.dev.br>
2024-03-26 16:42:12 -0300

    Refactor data saving logic in Saver class

--- app/features/fetcher/utils/saver.py
+++ app/features/fetcher/utils/saver.py
@@ -3,7 +3,11 @@ class Saver:
     @classmethod
     def save_proposition_data(cls, data):
         data_saving = {
-            "ementa": data["dados"]["ementa"] if "ementa" in data["dados"] else None,
+            "ementa": (
+                data["dados"]["ementa"]
+                if "ementa" in data["dados"]
+                else None if "dados" in data else None
+            ),
             "ementaDetalhada": (
                 data["dados"]["ementaDetalhada"]
                 if "ementaDetalhada" in data["dados"]
@@ -12,23 +16,59 @@ class Saver:
             "keywords": data["dados"]["keywords"],
             "uris": {
                 "uriAutores": data["dados"]["uriAutores"],
-                "uriPropPrincipal": data["dados"]["uriPropPrincipal"] if "uriPropPrincipal" in data["dados"] else None,
-                "uriUltimoRelator": data["dados"]["statusProposicao"]["uriUltimoRelator"] if "statusProposicao" in data["dados"] else None,
+                "uriPropPrincipal": (
+                    data["dados"]["uriPropPrincipal"]
+                    if "uriPropPrincipal" in data["dados"]
+                    else None
+                ),
+                "uriUltimoRelator": (
+                    data["dados"]["statusProposicao"]["uriUltimoRelator"]
+                    if "statusProposicao" in data["dados"]
+                    else None
+                ),
             },
         }
         return data_saving
 
     @classmethod
     def save_author_data(cls, data):
-        data_saving = {
-            "nome": data["dados"]["nomeCivil"] if "nomeCivil" in data["dados"] else data["dados"]["nome"],
-            "cpf": data["dados"]["cpf"] if "cpf" in data["dados"] else None,
-            "foto": data["dados"]["ultimoStatus"]["urlFoto"] if "ultimoStatus" in data["dados"] else None,
-            "sexo": data["dados"]["sexo"] if "sexo" in data["dados"] else None,
-            "nascimento": data["dados"]["dataNascimento"] if "dataNascimento" in data["dados"] else None,
-            "local_nascimento": f"{data['dados']['municipioNascimento']} - {data['dados']['ufNascimento']}" if "municipioNascimento" in data["dados"] else None,
-            "escolaridade": data["dados"]["escolaridade"] if "escolaridade" in data["dados"] else None,
-        }
+        if "dados" in data:
+            data_saving = {
+                "nome": (
+                    data["dados"]["nomeCivil"]
+                    if "nomeCivil" in data["dados"]
+                    else data["dados"]["nome"]
+                ),
+                "cpf": data["dados"]["cpf"] if "cpf" in data["dados"] else None,
+                "foto": (
+                    data["dados"]["ultimoStatus"]["urlFoto"]
+                    if "ultimoStatus" in data["dados"]
+                    else None
+                ),
+                "sexo": data["dados"]["sexo"] if "sexo" in data["dados"] else None,
+                "nascimento": (
+                    data["dados"]["dataNascimento"]
+                    if "dataNascimento" in data["dados"]
+                    else None
+                ),
+                "local_nascimento": (
+                    f"{data['dados']['municipioNascimento']} - {data['dados']['ufNascimento']}"
+                    if "municipioNascimento" in data["dados"]
+                    else None
+                ),
+                "escolaridade": (
+                    data["dados"]["escolaridade"]
+                    if "escolaridade" in data["dados"]
+                    else None
+                ),
+            }
+        else:
+            data_saving={
+                'nome': 'Não informado',
+                'cpf': 'Não informado',
+                'orgão': 'Sim',
+            }
+        
         return data_saving
 
     @classmethod
